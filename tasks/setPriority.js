b6253c885356dab1636f275f2ddc0764e6be8000
pete <peteryuhsin@gmail.com>
2022-11-02 19:07:43 +0800

    Resolve #13

--- tasks/setPriority.js
+++ tasks/setPriority.js
@@ -1,14 +1,15 @@
-import { checkHighPriorityJobs, checklowhPriorityJobs } from "../dbConnection/getJobs.js";
+import {
+  checkHighPriorityJobs,
+  checklowhPriorityJobs,
+} from "../dbConnection/getJobs.js";
 
 const setPriority = async (pool, client) => {
-    let priority = -1;
+  if ((await checkHighPriorityJobs(pool, client)).length != 0) {
+    return 0;
+  }
+  if ((await checklowhPriorityJobs(pool, client)).length != 0) {
+    return 1;
+  }
+};
 
-    if((await checkHighPriorityJobs(pool, client)).length != 0){
-        return priority = 0;
-    }else if((await checklowhPriorityJobs(pool, client)).length != 0){
-        return priority = 1;
-    }
-  };
-
-
-  export{setPriority}
+export { setPriority };
